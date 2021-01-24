2b5a306758f578fa2b1c3b7a8977d8c2a41670be
SukruMertOz <77850643+SukruMertOz@users.noreply.github.com>
2021-01-24 18:11:48 +0300

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> citys = new List<string>();
+            citys.Add("Newyork");
+            citys.Add("Newyork");
+            citys.Add("Newyork");
+            citys.Add("Newyork");
+            citys.Add("Newyork");
+            citys.Add("Newyork");
+            citys.Add("Newyork");
+
+            Console.WriteLine(citys.Count);
+
+            MyList <string> citys2 = new MyList<string>();
+            
+            citys2.Add("Ankara");
+            citys2.Add("Ankara");
+            citys2.Add("Ankara");
+            citys2.Add("Ankara");
+            citys2.Add("Ankara");
+            citys2.Add("Ankara");
+            citys2.Add("Ankara");
+            citys2.Add("Ankara");
+            citys2.Add("Ankara");
+            citys2.Add("Ankara");
+            citys2.Add("Ankara");
+            Console.WriteLine(citys2.Count);
+        }
+    }
+    class MyList<T> //Generic Class
+    {
+        T[] _array;
+        T[] _tempArray;
+        public MyList()
+        {
+            _array = new T[0];
+           
+        }
+        public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+            for (int i = 0; i <_tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+
+            _array[_array.Length - 1] = item;
+        }
+
+        public int Count
+        {
+            get { return _array.Length; }
+
+        }
+    }
+   
+
+}
